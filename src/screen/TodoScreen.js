9ccbd7e42cbc0020a94b6faa4af7ee80bfd21790
Aslınur Tolga <aslinurtolga@metaworldbilisim.com>
2024-01-31 18:02:26 +0300

    first commit

---
+++ src/screen/TodoScreen.js
@@ -0,0 +1,90 @@
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { IconButton } from "react-native-paper";
+
+const dummyData = [
+  {
+    id: "01",
+    title: "Wash Car",
+  },
+  {
+    id: "02",
+    title: "Read A book",
+  },
+];
+const TodoScreen = () => {
+  // init local states
+  const [todo, setTodo] = useState("");
+
+  // render todo
+  const renderTodos = ({ item, index }) => {
+    return (
+      <View
+        style={{
+          backgroundColor: "#1e90ff",
+          borderRadius: 6,
+          paddingHorizontal: 6,
+          paddingVertical: 12,
+          marginBottom: 12,
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <IconButton icon="pencil" iconColor="#fff" />
+        <IconButton icon="trash-can" iconColor="#fff" />
+        <Text
+          style={{ color: "#fff", fontSize: 20, fontWeight: "800", flex: 1 }}
+        >
+          {item.title}
+        </Text>
+      </View>
+    );
+  };
+  return (
+    <View style={{ marginHorizontal: 16 }}>
+      <Text>TodoScreen</Text>
+
+      <TextInput
+        style={{
+          borderWidth: 2,
+          borderColor: "#99BC85",
+          borderRadius: 6,
+          paddingVertical: 12,
+          paddingHorizontal: 16,
+        }}
+        placeholder="Add a task"
+        value={todo}
+        onChangeText={(userText) => setTodo(userText)}
+      />
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#000",
+          borderRadius: 6,
+          paddingVertical: 8,
+          marginVertical: 34,
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "#fff", fontWeight: "bold", fontSize: "20" }}>
+          Add
+        </Text>
+      </TouchableOpacity>
+
+      {/* Render To Do List */}
+
+      <FlatList data={dummyData} renderItem={renderTodos} />
+    </View>
+  );
+};
+
+export default TodoScreen;
+
+const styles = StyleSheet.create({});
